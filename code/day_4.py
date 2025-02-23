e608fb00275954a8d8a04ec32c3b758ece5748c8
Asif Sayyed <asifdotexe@gmail.com>
2025-02-23 19:22:19 +0530

    #7 loaded all the datasets

--- code/day_4.py
+++ code/day_4.py
@@ -13,9 +13,18 @@ import plotly.express as px
 import streamlit as st
 from datetime import datetime
 
-# loading the dataset
+# loading the aggregated metric by video dataset
 aggregated_metric_by_video = pd.read_csv(
-    "data/ken-jee-yt-data/Aggregated_Metrics_By_Video.csv")
+    "data/ken-jee-yt-data/Aggregated_Metrics_By_Video.csv"
+)
 # skipping the first row and including everything else
 aggregated_metric_by_video = aggregated_metric_by_video.iloc[1:,:]
 
+# loading aggregated metric by country and subscriber status dataset
+aggregated_metric_by_subs = pd.read_csv(
+    "data/ken-jee-yt-data/Aggregated_Metrics_By_Country_And_Subscriber_Status.csv"
+)
+
+# loading the comment and time series dataset
+comment_data = pd.read_csv("data/ken-jee-yt-data/All_Comments_Final.csv")
+time_series_data = pd.read_csv("data/ken-jee-yt-data/Video_Performance_Over_Time.csv")
