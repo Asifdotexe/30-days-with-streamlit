9bf4620bafb5c476af02c3189c6cb8679f29d859
Asif Sayyed <asifdotexe@gmail.com>
2025-02-23 19:17:46 +0530

    #7 reading the data

--- code/day_4.py
+++ code/day_4.py
@@ -5,3 +5,17 @@ link: https://www.kaggle.com/datasets/kenjee/ken-jee-youtube-data
 
 @author: Asif Sayyed
 """
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import streamlit as st
+from datetime import datetime
+
+# loading the dataset
+aggregated_metric_by_video = pd.read_csv(
+    "data/ken-jee-yt-data/Aggregated_Metrics_By_Video.csv")
+# skipping the first row and including everything else
+aggregated_metric_by_video = aggregated_metric_by_video.iloc[1:,:]
+
